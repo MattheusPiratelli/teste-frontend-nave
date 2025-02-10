e2460effd0a40e02a66ffc2f40fcd7d4f5e8ece8
MattheusPiratelli <mattheus_17@hotmail.com>
2025-02-10 20:30:41 -0300

    Tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 function App() {
   return <div>Hello World</div>;
+  
 }
 
 export default App;
---
+++ src/Login/index.jsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export const Login = () => {
+  return (
+    <div className="login_container">
+      <div className="login_card">Login</div>
+    </div>
+  );
+};
