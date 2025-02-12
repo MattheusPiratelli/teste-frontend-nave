57451b0106f23bce1b4f99dec12e0cb286d709c2
MattheusPiratelli <mattheus_17@hotmail.com>
2025-02-12 16:39:50 -0300

    login finalizado

--- src/Login/index.jsx
+++ src/Login/index.jsx
@@ -3,7 +3,22 @@ import "./styles.css";
 export const Login = () => {
   return (
     <div className="login_container">
-      <div className="login_card">Login</div>
+      <div className="login_card">
+        <img src="/logo.png" alt="Logo Nave.RS" />
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input id="email" type="text" />
+        </div>
+
+        <div>
+          <label htmlFor="senha">Senha</label>
+          <input id="senha" type="text" />
+        </div>
+        
+        <div>
+          <button className="primary">Entrar</button>
+        </div>
+      </div>
     </div>
   );
 };
