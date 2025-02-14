b8c4647e9d5d6359304cfb918053d4d0e0567757
MattheusPiratelli <mattheus_17@hotmail.com>
2025-02-13 22:40:28 -0300

    Dashboard

---
+++ src/Header/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+
+export const Header = () => {
+  return (
+    <div className="header_container">
+      <img src="/logo.png" alt="logo" />
+      <button>Sair</button>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-// import App from './App.jsx'
-import { Login } from './Login'
+// import { Login } from './Login'
+import { Dashboard } from "./Dashboard";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Login />
-  </StrictMode>,
-)
+    {/* <Login /> */}
+    <Dashboard />
+  </StrictMode>
+);
