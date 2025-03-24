f784144025e292f36104bd15c85808a48f83cb93
MattheusPiratelli <mattheus_17@hotmail.com>
2025-03-24 20:13:00 -0300

    Avatar Modal

---
+++ src/Delete/index.jsx
@@ -0,0 +1,43 @@
+import "./styles.css";
+import { useNavigate } from "react-router";
+import { Header } from "../Header";
+import { Input } from "../Input";
+import { Modal } from "../Modal";
+import { useState } from "react";
+
+export const Delete = () => {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/dashboard");
+  };
+
+  const handleSave = () => {
+    setOpen(true);
+  };
+
+  return (
+    <div className="register_container">
+      <Modal open={open} setOpen={setOpen} type={"NaverDelete"} />
+      <Header />
+      <div className="register_wrapper">
+        <button className="register_goback" onClick={handleClick}>
+          <img src="./return.png" alt="voltar" />
+          <h1 className="add"> Editar Naver </h1>
+        </button>
+        <div className="register_inputs">
+          <Input label={"Nome"} />
+          <Input label={"Cargo"} />
+          <Input label={"Idade"} />
+          <Input label={"Tempo de empresa"} />
+          <Input label={"Projetos que participou"} />
+          <Input label={"URL da foto do Naver"} />
+        </div>
+        <div className="register_save">
+          <button onClick={handleSave}>Salvar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Modal/NaverAvatarModal/index.jsx
+++ src/Modal/NaverAvatarModal/index.jsx
@@ -6,8 +6,19 @@ export const NaverAvatarModal = ({ setOpen }) => {
   };
 
   return (
-    <div className="naver_avatar_modal">
-      <h1>AVATAR</h1>
+    <div className="naver_avatar_modal">      
+      <img className="avatar" src="/avatar_1_1.png" alt="" />
+      <h1>Juliano Reis</h1>
+      <p>Front-end Developer</p>
+      <h2>Idade</h2>
+      <p>Lorem Ipsum</p>
+      <h2>Tempo de empresa</h2>
+      <p>Lorem Ipsum</p>
+      <h2>Projetos que participou</h2>
+      <p>Lorem Ipsum</p>
+      <img className="lixeira" src="/lixeira.png" alt="lixeira" />
+      <img className="editar" src="/editar.png" alt="editar" />
     </div>
+    
   );
 };
